87290761e99e4a030fad2714ffddeb3768a610b8
Тансылу Ахметова <45488234+tansylu@users.noreply.github.com>
2019-03-17 19:14:59 +0300

    Update start.py

--- start.py
+++ start.py
@@ -5,12 +5,12 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-Message = namedtuple('Message', 'text tag')
+Message = namedtuple('Message', 'text tag username')
 messages = []
 
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def hello():
     return render_template('index.html')
 
 
@@ -23,9 +23,12 @@ def main():
 def add_message():
     text = request.form['text']
     tag = request.form['tag']
+    username = request.form['username']
 
-    messages.append(Message(text, tag))
+    messages.append(Message(text, tag, username))
 
     return redirect(url_for('main'))
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
